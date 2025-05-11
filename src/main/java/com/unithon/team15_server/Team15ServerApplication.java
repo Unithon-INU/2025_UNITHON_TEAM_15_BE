95c40e3d410e287025cdd1f022be1b28bd43671e
jiyunio <jiyun3269@naver.com>
2025-05-11 18:14:53 +0900

    first commit

---
+++ src/main/java/com/unithon/team15_server/Team15ServerApplication.java
@@ -0,0 +1,13 @@
+package com.unithon.team15_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team15ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team15ServerApplication.class, args);
+	}
+
+}
