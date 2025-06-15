fd69523c778267d8c096a89b65cc703b11ac0d70
jiyunio <jiyun3269@naver.com>
2025-06-16 00:50:46 +0900

    [Refactor] repository 어노테이션 제거 #1

--- src/main/java/com/unithon/team15_server/domain/member/MemberRepository.java
+++ src/main/java/com/unithon/team15_server/domain/member/MemberRepository.java
@@ -3,11 +3,9 @@ package com.unithon.team15_server.domain.member;
 import com.unithon.team15_server.domain.member.dto.MemberProfileGetRes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
