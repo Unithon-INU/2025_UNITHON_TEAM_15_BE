6eb681b64c1ef1a87d5745c7ae8f9a723efd7e89
jiyunio <jiyun3269@naver.com>
2025-06-18 16:37:48 +0900

    [Docs] API 명세서 response 수정

--- src/main/java/com/unithon/team15_server/domain/employmentcheck/EmploymentCheckController.java
+++ src/main/java/com/unithon/team15_server/domain/employmentcheck/EmploymentCheckController.java
@@ -27,14 +27,14 @@ public class EmploymentCheckController {
 
     @Operation(summary = "내 시간제취업 정보 전체 조회", description = "- 모든 step에 대한 체크리스트 정보를 전달\n" +
             "- step의 정보와 유의사항 전달\n")
-    @ApiResponse(responseCode = "200", description = "내 시간제취업 정보 전체 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = HomeInfoRes.class))))
+    @ApiResponse(responseCode = "200", description = "내 시간제취업 정보 전체 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = HomeInfoRes.class)))
     @GetMapping("/home")
     public ResponseEntity<HomeInfoRes> getHomeInfo(@AuthenticationPrincipal MemberDetail memberDetail) {
         return ResponseEntity.ok(employmentCheckService.getHomeInfo(memberDetail.getId()));
     }
 
     @Operation(summary = "Tip 정보 조회", description = "- step에 대한 tip 정보 조회")
-    @ApiResponse(responseCode = "200", description = "Tip 정보 조회 성공", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = TipInfoRes.class))))
+    @ApiResponse(responseCode = "200", description = "Tip 정보 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TipInfoRes.class)))
     @GetMapping("/tips")
     public ResponseEntity<List<TipInfoRes>> getTips(@RequestParam CheckStep checkStep) {
         return ResponseEntity.ok(employmentCheckService.getTipInfo(checkStep));
