e3cd528e59fbac310b814efdd918e741f364f5b9
jiyunio <jiyun3269@naver.com>
2025-10-27 20:20:50 -0700

    [Chore] aws healthy check 의존성 추가

--- src/main/java/com/unithon/team15_server/Team15ServerApplication.java
+++ src/main/java/com/unithon/team15_server/Team15ServerApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 })
 @SpringBootApplication
 public class Team15ServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Team15ServerApplication.class, args);
 	}
