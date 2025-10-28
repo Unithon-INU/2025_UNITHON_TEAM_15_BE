da67d7dcb6a676df16887ced80796aecf468fd49
jiyunio <jiyun3269@naver.com>
2025-10-27 19:57:38 -0700

    [Refactor] swagger에서 https로 요청을 보내도록 설정

--- src/main/java/com/unithon/team15_server/Team15ServerApplication.java
+++ src/main/java/com/unithon/team15_server/Team15ServerApplication.java
@@ -1,8 +1,13 @@
 package com.unithon.team15_server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Default Server URL")
+})
 @SpringBootApplication
 public class Team15ServerApplication {
 
