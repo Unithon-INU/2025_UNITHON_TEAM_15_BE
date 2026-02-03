b4f42e83c27e8113091d8ca29a1850d1626b27af
jiyunio <jiyun3269@naver.com>
2026-02-03 21:21:18 +0900

    [Feat] 외부 API 사용을 위한 OpenFeign 의존성 추가 및 설정

--- src/main/java/com/unithon/team15_server/Team15ServerApplication.java
+++ src/main/java/com/unithon/team15_server/Team15ServerApplication.java
@@ -4,14 +4,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @OpenAPIDefinition(servers = {
-		@Server(url = "/", description = "Default Server URL")
+        @Server(url = "/", description = "Default Server URL")
 })
 @SpringBootApplication
+@EnableFeignClients
 public class Team15ServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(Team15ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Team15ServerApplication.class, args);
+    }
 
 }
