8c3eb29a97387534bcea1e22129a749a73631655
jiyunio <jiyun3269@naver.com>
2026-02-12 01:17:02 +0900

    [Refactor] ErrorCode enum을 더 정확한 이름으로 변경

--- src/main/java/com/unithon/team15_server/global/exception/ErrorCode.java
+++ src/main/java/com/unithon/team15_server/global/exception/ErrorCode.java
@@ -10,14 +10,15 @@ public enum ErrorCode {
     //400 Bad Request
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "400-1", "입력값이 올바르지 않습니다."),
     MISSING_REQUIRED_FIELD(HttpStatus.BAD_REQUEST, "400-2", "요청 값이 누락되었습니다."),
+    INVALID_CODE(HttpStatus.BAD_REQUEST, "400-3", "유효하지 않은 코드입니다."),
 
     //401 Unauthorized
     ERROR_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "401-1", "인증 정보가 없습니다."),
     INVALID_TOKEN_FORMAT(HttpStatus.UNAUTHORIZED, "401-2", "토큰의 형식이 유효하지 않습니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "401-3", "토큰 기간이 만료됐습니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "401-5", "비밀번호가 일치하지 않습니다."),
-    INVALID_CODE(HttpStatus.UNAUTHORIZED, "401-6", "인증코드가 유효하지 않습니다."),
-    EXPIRED_CODE(HttpStatus.UNAUTHORIZED, "401-7", "인증코드가 만료되었습니다."),
+    INVALID_AUTH_CODE(HttpStatus.UNAUTHORIZED, "401-6", "인증코드가 유효하지 않습니다."),
+    EXPIRED_AUTH_CODE(HttpStatus.UNAUTHORIZED, "401-7", "인증코드가 만료되었습니다."),
 
     //403 Forbidden
 
@@ -27,7 +28,10 @@ public enum ErrorCode {
 
     //409 Conflict
     USER_ALREADY_REGISTERED(HttpStatus.CONFLICT, "409-1", "이미 존재하는 회원입니다.."),
-    NICKNAME_ALREADY_REGISTERED(HttpStatus.CONFLICT, "409-2", "이미 등록된 닉네임입니다.");
+    NICKNAME_ALREADY_REGISTERED(HttpStatus.CONFLICT, "409-2", "이미 등록된 닉네임입니다."),
+
+    //500 Internal Server Error
+    ACCREDITED_UNI_FILE_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "500-1", "인증대학 데이터 파일을 찾을 수 없습니다.");
 
 
     private final HttpStatus httpStatus;
