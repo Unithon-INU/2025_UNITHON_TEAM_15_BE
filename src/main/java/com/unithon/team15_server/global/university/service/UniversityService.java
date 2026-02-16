813fdf8d63ecf6faf0b12876f56bde2d49b2e880
jiyunio <jiyun3269@naver.com>
2026-02-16 23:02:13 +0900

    [Feat] 대학교명 및 학과 영문 번역 기능 추가 - DeepL API를 사용한 번역 기능 구현 - 최초 번역 시 Redis에 캐싱하여 이후 번역 요청 성능 최적화
    #26

--- src/main/java/com/unithon/team15_server/global/university/service/UniversityService.java
+++ src/main/java/com/unithon/team15_server/global/university/service/UniversityService.java
@@ -1,10 +1,14 @@
 package com.unithon.team15_server.global.university.service;
 
-import com.unithon.team15_server.global.university.dto.UniversityRes;
+import com.unithon.team15_server.global.translate.TranslateRedisRepository;
+import com.unithon.team15_server.global.translate.Translation;
+import com.unithon.team15_server.global.translate.Translator;
 import com.unithon.team15_server.global.university.dto.UniversityMajorsRes;
+import com.unithon.team15_server.global.university.dto.UniversityRes;
 import com.unithon.team15_server.global.university.entity.University;
 import com.unithon.team15_server.global.university.repository.UniversityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,8 +19,18 @@ import java.util.stream.Collectors;
 public class UniversityService {
 
     private final UniversityRepository universityRepository;
+    private final TranslateRedisRepository translateRedisRepository;
+    private final Translator translator;
 
     public List<UniversityRes> searchByUniversity(String university) {
+        String lang = LocaleContextHolder.getLocale().getLanguage();
+        final boolean isEnglish = lang.equals("en");
+
+        if (isEnglish) {
+            university = translator.translateToKorean(university);
+        }
+
+
         List<University> results = universityRepository
                 .findByUniversityContaining(university);
 
@@ -27,16 +41,40 @@ public class UniversityService {
                 .map(group -> {
                     University first = group.get(0);
                     return UniversityRes.builder()
-                            .university(first.getUniversity())
+                            .university(isEnglish ? getUniversityTranslation(first.getUniversity()) : first.getUniversity())
                             .universityType(first.getUniversityType())
                             .majors(group.stream()
                                     .map(u -> UniversityMajorsRes.builder()
-                                            .major(u.getMajor())
+                                            .major(isEnglish ? getMajorTranslation(u.getMajor()) : u.getMajor())
                                             .studyPeriod(u.getStudyPeriod())
                                             .build())
                                     .toList())
                             .build();
                 })
                 .toList();
+    }
+
+    private String getUniversityTranslation(String kor) {
+        return translateRedisRepository.findById("university:" + kor)
+                .map(Translation::getEng)
+                .orElseGet(
+                        () -> {
+                            String eng = translator.translateToEnglish(kor);
+                            translateRedisRepository.save(Translation.ofUniversity(kor, eng));
+                            return eng;
+                        }
+                );
+    }
+
+    private String getMajorTranslation(String kor) {
+        return translateRedisRepository.findById("major:" + kor)
+                .map(Translation::getEng)
+                .orElseGet(
+                        () -> {
+                            String eng = translator.translateToEnglish(kor);
+                            translateRedisRepository.save(Translation.ofMajor(kor, eng));
+                            return eng;
+                        }
+                );
     }
 }
